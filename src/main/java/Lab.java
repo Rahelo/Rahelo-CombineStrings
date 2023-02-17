3df66392a55fac154e0c9e099e08507c9fe5a95b
Rahelo <rahelsah9@gmail.com>
2023-02-17 08:22:26 +0000

    lab done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -8,7 +8,12 @@ public class Lab {
      * @param b the second part of the string to be combined.
      * @return the combination of strings a and b.
      */
+      
     public String combine(String a, String b){
-        return "";
-    }
+                      
+        return a +  b;
+       
 }
+    
+}
+
